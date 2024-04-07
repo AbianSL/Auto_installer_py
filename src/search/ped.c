fc99ec78790db001a479e01df830448ccf9ae928
AbianSL <abisanle@gmail.com>
2024-04-07 09:40:59 +0200

    add the ped.c to optimize

---
+++ src/search/ped.c
@@ -0,0 +1,12 @@
+/**
+* @name PED
+* @version 1.0
+* @description calculate the proximity edit distance between two strings
+* @date 2024-04-07
+* @param {string} string_1 - the first string
+* @param {string} string_2 - the second string
+* @returns {number} the proximity edit distance between the two strings
+*
+*/
+
+
