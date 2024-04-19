7324e9857833fd093e6662e6a1f436fc728c99b9
Abian <alu0101487458@ull.edu.es>
2024-04-19 13:54:49 +0200

    starting the next classes

--- src/user_menu/main_menu.py
+++ src/user_menu/main_menu.py
@@ -2,4 +2,13 @@
 # author: AbianSL (Abian Santana Ledesma)
 # Date: 2024-04-19
 # Version: 0.1
-# Description: contain a class to manage the interface 
+# Description: contain a class to manage the interface
+
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import menu
+
+class main_interface(general_menu)
