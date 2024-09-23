9d235db2e2f153a7d91b42676c87029aa1542c0d
Abian <91571820+AbianSL@users.noreply.github.com>
2024-09-23 10:12:27 +0000

    restructuring to start without 'pretty' interface

--- src/menu.py
+++ src/menu.py
@@ -4,8 +4,6 @@
 # Version: 0.1
 # Description: contain a class to manage the interface 
 
-import curses
-
 class general_menu:
     def __init__(self, items: list) -> None:
         self.items = items
@@ -13,17 +11,9 @@ class general_menu:
     
     def display(self, stdscr) -> None:
         stdscr.clear()
-        height, width = stdscr.getmaxyx()
-
-        for idx, item in enumerate(self.items):
-            x_coordinate = width//2 - len(item)//2
-            y_coordinate = height//2 - len(self.items)//2 + idx
-
-            if idx == self.selected:
-                stdscr.attron(curses.color_pair(1))
-                stdscr.addstr(y_coordinate, x_coordinate, item)
-                stdscr.attroff(curses.color_pair(1))
+        for index, item in enumerate(self.items):
+            if index == self.selected:
+                stdscr.addstr(index, 0, f"> {item}")
             else:
-                stdscr.addstr(y_coordinate, x_coordinate, item)
-        
+                stdscr.addstr(index, 0, f"  {item}")
         stdscr.refresh()
